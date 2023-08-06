4de9b2eb2ee9943d12abb802b931670b6929dd83
Irina Goncharova <irina.v.goncharova@gmail.com>
2023-08-06 03:26:02 +0300

    add txt template

---
+++ data/txt.py
@@ -0,0 +1,14 @@
+template = r"""
+<b>{name}</b>
+<b>Город</b>: #{area}
+<b>Формат работы</b>: {employment}, {schedule}
+<b>Зарплата</b>: от {salary_from}{salary_to} руб.
+<b>Требуемый опыт:</b> {experience}
+
+{description}
+
+<strong>Откликнуться</strong>
+{contacts_name}{contacts_email}{contacts_phones}
+{professional_roles}
+{apply_alternate_url}
+"""
